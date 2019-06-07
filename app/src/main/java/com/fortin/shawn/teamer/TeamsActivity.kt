a61a74582d07e57ee0f106934d438b75bcad0947
shawnfortin <shawn.fortin@mymail.champlain.edu>
2019-06-07 13:10:24 -0400

    added Teams activity and fragment

---
+++ app/src/main/java/com/fortin/shawn/teamer/TeamsActivity.kt
@@ -0,0 +1,11 @@
+package com.fortin.shawn.teamer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TeamsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teams)
+    }
+}
